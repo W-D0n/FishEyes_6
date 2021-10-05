60c48e5b14561f7ad586b8d6ade786baa6a0fb2b
Kamal <71557306+D0natellll0@users.noreply.github.com>
2021-10-05 15:34:33 +0400

    Commit 00.00.03c
    Fix config and files

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-import {} from 'src/js/modules/profilStackAPI.js'
-import {} from 'src/js/modules/form.js'
-import {} from 'src/js/modules/modal.js'
-import {} from 'src/js/modules/navigation.js'
-=======
-import {} from './src/js/modules/profilStackAPI'
-import {} from './src/js/modules/form'
-import {} from './src/js/modules/modal'
-import {} from './src/js/modules/navigation'
->>>>>>> master
+import {} from './modules/profilStackAPI.js'
+import {} from './modules/form.js'
+import {} from './modules/modal.js'
+import {} from './modules/navigation.js'
